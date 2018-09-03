00f5000d40f405b09c70f1bda5e93dd6de7e5fc8
pogrems <42866128+pogrems@users.noreply.github.com>
2018-09-03 10:55:03 +0300

    Update month_range.js

--- month_range.js
+++ month_range.js
@@ -1,4 +1,4 @@
-var fund = function(i) {
+var fund = function() {
     var date = new Date();          //gets current date
     var month = date.getMonth();    //shows a current month
     var dow = function(d){          //returns day of week 
@@ -10,27 +10,16 @@ var fund = function(i) {
     var a = 0;                      // default amount of working days this month
     var res = document.getElementById('res'); // div with results
     var descr = document.getElementById('description');//div with description phrase
-    var c_hours = document.getElementById('c_hours');
-    //var result = document.createElement('div'); // div with counted hours
-                        // total hours
-
-    var h = a*8;
+    var c_hours = document.getElementById('c_hours'); // div with counted hours
+    //var h = a*8;
+    
     for (i = 1; i<day;i++){         // cycle that counts amount of working days to current day
         if(dow(i) in [0,7])
             {continue}
         a = i;
     }
     
-         return h;
-    c_hours.innerHTML = h.toLocaleString(); // should insert result div to main div
+        a = a*8;
+         return a;
+    c_hours.innerHTML = a.toLocaleString(); // should insert result div to main div
 }
-
-var testAdd = function (){
-    var old_div = document.getElementById('testdiv');
-    var old = document.getElementById('old');
-    var neu = document.createElement('div'); //creates div with counted hours
-    neu.innerHTML = 'added text';            //writes hours to div
-    old_div.insertBefore(neu, old);          //puts the hours div into description div 
-}
-
-
